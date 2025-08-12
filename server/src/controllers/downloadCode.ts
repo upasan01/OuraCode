e9914cb3a66cbd925101d56ab8fc5195b1bd9a58
SANJOY <paulsanjoy2923@gmail.com>
2025-08-12 11:39:24 +0530

    download route beta

--- server/src/controllers/downloadCode.ts
+++ server/src/controllers/downloadCode.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
-import { redis } from "../lib/redis.js";
 
 export const downloadCode = async (req: Request, res: Response) => {
-    res.json({
-        message: "Working ig"
-    })
+    try {
+        const { code, languagae } = req.body
+
+        if (!code) {
+            return res.status(404).json({
+                message: "Can not download empty page",
+                code: 404
+            })
+        }
+
+        const fileName = `code.${languagae || "txt"}`
+
+        res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    } catch (err) {
+        return res.status(500).json({
+            message: "Something Went Wrong",
+            //@ts-ignore
+            error: err.message,
+            code: 500
+        })
+    }
 }
