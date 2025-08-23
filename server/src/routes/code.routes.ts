e55858a348cad135ec5e75eaf32603779241bf06
SANJOY <paulsanjoy2923@gmail.com>
2025-08-23 10:49:22 +0530

    RIP MongoDB ---> welcome redis

--- server/src/routes/code.routes.ts
+++ server/src/routes/code.routes.ts
@@ -3,12 +3,10 @@
 import { Router } from "express";
 import { saveCode } from "../controllers/saveCode.js";
 import { downloadCode } from "../controllers/downloadCode.js";
-import { runCode } from "../controllers/runCode.js";
 
 const codeRouter = Router()
 
 codeRouter.post("/save", saveCode)
 codeRouter.post("/download", downloadCode)
-codeRouter.post("/run", runCode)
 
 export default codeRouter
